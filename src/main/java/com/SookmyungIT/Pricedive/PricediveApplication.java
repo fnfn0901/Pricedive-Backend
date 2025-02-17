977cf74a2fe8a198f66b29baed8beb512bf9b735
hoyeon <fnfn0901@sookmyung.ac.kr>
2025-02-17 11:02:58 +0900

    chore: .env 파일 구현 및 .gitignore 수정

--- src/main/java/com/SookmyungIT/Pricedive/PricediveApplication.java
+++ src/main/java/com/SookmyungIT/Pricedive/PricediveApplication.java
@@ -2,9 +2,18 @@ package com.SookmyungIT.Pricedive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PricediveApplication {
+	static {
+		Dotenv dotenv = Dotenv.configure()
+				.directory(System.getProperty("user.dir"))
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PricediveApplication.class, args);
